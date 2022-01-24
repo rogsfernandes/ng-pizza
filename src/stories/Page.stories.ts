b05913b030e778a8dd02021b2e28eb8a0f4843d1
Roger Santos <rsfernandes@outlook.com>
2022-01-23 22:03:24 -0300

    Adds storybook

---
+++ src/stories/Page.stories.ts
@@ -0,0 +1,35 @@
+// import { moduleMetadata, Story, Meta } from '@storybook/angular';
+// import { CommonModule } from '@angular/common';
+
+// import Button from './button.component';
+
+// import Page from './page.component';
+
+// import * as HeaderStories from './Header.stories';
+// import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+
+// export default {
+//   title: 'Example/Page',
+//   component: Page,
+//   decorators: [
+//     moduleMetadata({
+//       declarations: [Button, HeaderComponent],
+//       imports: [CommonModule],
+//     }),
+//   ],
+// } as Meta;
+
+// const Template: Story<Page> = (args: Page) => ({
+//   props: args,
+// });
+
+// export const LoggedIn = Template.bind({});
+// LoggedIn.args = {
+//   // More on composing args: https://storybook.js.org/docs/angular/writing-stories/args#args-composition
+//   ...HeaderStories.LoggedIn.args,
+// };
+
+// export const LoggedOut = Template.bind({});
+// LoggedOut.args = {
+//   ...HeaderStories.LoggedOut.args,
+// };
