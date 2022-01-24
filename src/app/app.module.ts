7c05958e1ff6cfe60de744510964586ea84c9913
Roger Santos <rsfernandes@outlook.com>
2022-01-23 21:20:47 -0300

    Adds project generated with ng cli

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from "./shared/templates/layout.component";
+import { HeaderComponent } from './shared/components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/templates/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-layout.component.ts",
+  styles: [],
+  template: ``,
+})
+export class LayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
