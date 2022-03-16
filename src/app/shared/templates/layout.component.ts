4b5f18dead7ac890d346c30e12d2b64451b5908c
Roger Santos <rsfernandes@outlook.com>
2022-03-16 11:36:02 -0300

    feat(layout): add grid to layout

--- src/app/shared/templates/layout.component.ts
+++ src/app/shared/templates/layout.component.ts
@@ -2,10 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mia-layout',
-  styles: [],
+  styles: [
+    `
+      :host {
+        display: grid;
+        grid-template: 10rem 1fr/ 4fr 1fr;
+        grid-template-areas: "top top" "main side";
+      }
+      .headerblock {
+        grid-area: top;
+      }
+      .sideblock {
+        background-color: aqua;
+        grid-area: side;
+      }
+      .contentblock {
+      }
+    `,
+  ],
   template: `
-    <mia-header></mia-header>
-    <ng-content></ng-content>
+    <mia-header class="headerblock"></mia-header>
+    <ng-content class="contentblock"></ng-content>
+    <div class="sideblock">sidebar</div>
   `,
 })
 export class LayoutComponent implements OnInit {
